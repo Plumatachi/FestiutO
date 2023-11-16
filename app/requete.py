4717c22a521eeeb96e7ae57d861dfc82e20af2af
Blandeau Erwan <erwan.blandeau28@gmail.com>
2023-11-16 12:07:11 +0100

    creation de classe pour organiser les requetes

--- app/requete.py
+++ app/requete.py
@@ -6,12 +6,27 @@ cnx = ouvrir_connexion()
 
 def afficher_table(cnx, table):
     try:
-        list = []
+        res = []
         result = cnx.execute(text("SELECT * FROM " + table + ";"))
         for row in result:
             print(row)
-            list.append(row)
-        return list
+            res.append(row)
+        return res
     except:
-        print("Erreur lors de l'affichage de la table")
+        print( "La classe Requete a rencontré une erreur lors de l'affichage de la table " + table + ".")
         raise
+
+class Utilisateur:
+        def get_tout_utilisateurs(cnx):
+            try:
+                res = []
+                result = cnx.execute(text("SELECT * FROM SPECTATEUR;"))
+                for row in result:
+                    print(row)
+                    res.append(row)
+                return res
+            except:
+                print("La classe Requete a rencontré une erreur lors de l'affichage de tous les utilisateurs.")
+                raise
+
+Utilisateur.get_tout_utilisateurs(cnx)
