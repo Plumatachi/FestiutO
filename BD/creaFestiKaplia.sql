abd15333524a416cd41713ce2572a24ec3861fcc
Kaplia Elliot <o22202795@info21-22.iut45.univ-orleans.fr>
2023-11-16 11:38:35 +0100

    ajout BD final

---
+++ BD/creaFestiKaplia.sql
@@ -0,0 +1,323 @@
+CREATE TABLE `JOURNEE`(
+  `idJournee` int,
+  `nomJournee` VARCHAR(42),
+  `dateDebutJ` date,
+  `dateFinJ` date,
+  `idfestival` int,
+  PRIMARY KEY(`idJournee`)
+);
+
+CREATE TABLE `EVENEMENT`(
+  `idEvenement` int,
+  `dateDebutE` DATETIME,
+  `dateFinE` DATETIME,
+  `idScene` int,
+  `idJournee` int,
+  `idActivite` int,
+  `idConcert` int,
+  PRIMARY KEY(`idEvenement`)
+);
+
+CREATE TABLE `SCENE` (
+  `idScene` int,
+  `nomScene` VARCHAR(42),
+  `lieux` VARCHAR(42),
+  PRIMARY KEY(idScene)
+);
+
+CREATE TABLE `ACTIVITE` (
+  `idActivite` int,
+  `nomactivite` VARCHAR(42),
+  `privatitation` VARCHAR(42),
+  PRIMARY KEY ( `idActivite`)
+)  ;
+
+CREATE TABLE `CONCERT` (
+  `idConcert` int,
+  `demontage` int,
+  `montage` int,
+  PRIMARY KEY ( `idConcert`)
+)  ;
+
+CREATE TABLE `RESERVER` (
+  `idspectateur` int,
+  `idJournee` int,
+  PRIMARY KEY (`idspectateur`, `idJournee`)
+)  ;
+
+CREATE TABLE `BILLETEVENEMENT` (
+  `idspectateur` int,
+  `idEvenement` int,
+  PRIMARY KEY (`idspectateur`, `idEvenement`)
+)  ;
+
+CREATE TABLE `FESTIVAL` (
+  `idfestival` int,
+  `datedebut` date,
+  `datefin` date,
+  PRIMARY KEY (`idfestival`)
+)  ;
+
+
+CREATE TABLE `GROUPE` (
+  `idgroupe` int,
+  `nomDuGroupe` VARCHAR(42),
+  `description` VARCHAR(100),
+  `reseausocial` VARCHAR(100),
+  `photo` LONGBLOB,  
+  `nbpersonne` int,
+  PRIMARY KEY (`idgroupe`)
+)  ;
+
+CREATE TABLE `HEBERGEMENT` (
+  `idhebergement` int,
+  `nbplace` int,
+  `lieux` VARCHAR(100),
+  PRIMARY KEY (`idhebergement`)
+)  ;
+
+CREATE TABLE `INSTRUMENT` (
+  `idinstrument` int,
+  `nominstrupent` VARCHAR(42),  
+  PRIMARY KEY (`idinstrument`)
+)  ;
+
+CREATE TABLE `JOUE` (
+  `idgroupe` int,
+  `idinstrument` int,
+  `idMusicien` int,
+  PRIMARY KEY (`idgroupe`, `idinstrument`,`idMusicien`)
+)  ;
+
+CREATE TABLE `PRESENT` (
+  `idfestival` int,
+  `idgroupe` int,
+  `datearriver` date,
+  `datedepart` date,
+  PRIMARY KEY (`idfestival`, `idgroupe`)
+)  ;
+
+CREATE TABLE `STYLEDEMUSIQUE` (
+  `typeDeMusique` VARCHAR(42),
+  PRIMARY KEY(`typeDeMusique`)
+) ;
+
+CREATE TABLE `SPECTATEUR` (
+  `idspectateur` int,
+  `nom` VARCHAR(42),
+  `numerotel` VARCHAR(10),
+  `mail` VARCHAR(100),
+  `motsDePasse` varchar(20),
+  PRIMARY KEY (`idspectateur`)
+)  ;
+
+CREATE TABLE `STYLE` (
+  `idgroupe` int,
+  `typeDeMusique` VARCHAR(42),
+  PRIMARY KEY (`idgroupe` ,`typeDeMusique`)
+)  ;
+
+CREATE TABLE `DORMIR` (
+  `nbPersonneGroupe` int,
+  `dateArrivee` date,
+  `dateDepart` date,
+  `idgroupe` int,
+  `idhebergement` int,
+  PRIMARY KEY (`idgroupe`,`idhebergement`) 
+) ;
+
+CREATE TABLE `MUSICIEN` (
+  `idMusicien` int,
+  `nom` VARCHAR(42),
+  `adresseMail` VARCHAR(42),
+  `numeroTelMusicien` int,
+  PRIMARY KEY (`idMusicien`)
+) ;
+
+-- A demander au prof
+CREATE TABLE `RESSEMBLE` (
+  `typeDeMusique` VARCHAR(42),
+  `typeDeMusique2` VARCHAR(42),
+  PRIMARY KEY(`typeDeMusique`)
+);
+
+CREATE TABLE `PARTICIPE` (
+  `idEvenement` int,
+  `idGroupe` int,
+  PRIMARY KEY(`idEvenement`, `idGroupe`)
+);
+
+CREATE TABLE `APPARTIENT` (
+    `idGroupe` int,
+    `idMusicien` int,
+    PRIMARY KEY (`idGroupe`, `idMusicien`)
+);
+
+ALTER TABLE `JOURNEE` ADD FOREIGN KEY(`idfestival`) REFERENCES `FESTIVAL` (`idfestival`);
+ALTER TABLE `BILLETEVENEMENT` ADD FOREIGN KEY(`idEvenement`) REFERENCES `EVENEMENT` (`idEvenement`);
+ALTER TABLE `BILLETEVENEMENT` ADD FOREIGN KEY(`idspectateur`) REFERENCES `SPECTATEUR` (`idspectateur`);
+ALTER TABLE `RESERVER` ADD FOREIGN KEY (`idJournee`) REFERENCES `JOURNEE` (`idJournee`);
+ALTER TABLE `RESERVER` ADD FOREIGN KEY (`idspectateur`) REFERENCES `SPECTATEUR` (`idspectateur`);
+ALTER TABLE `JOUE` ADD FOREIGN KEY (`idinstrument`) REFERENCES `INSTRUMENT` (`idinstrument`);
+ALTER TABLE `JOUE` ADD FOREIGN KEY (`idgroupe`) REFERENCES `GROUPE` (`idgroupe`);
+ALTER TABLE `JOUE` ADD FOREIGN KEY (`idMusicien`) REFERENCES `MUSICIEN` (`idMusicien`);
+ALTER TABLE `PRESENT` ADD FOREIGN KEY (`idgroupe`) REFERENCES `GROUPE` (`idgroupe`);
+ALTER TABLE `PRESENT` ADD FOREIGN KEY (`idfestival`) REFERENCES `FESTIVAL` (`idfestival`);
+ALTER TABLE `STYLE` ADD FOREIGN KEY (`idgroupe`) REFERENCES `GROUPE` (`idgroupe`);
+ALTER TABLE `STYLE` ADD FOREIGN KEY (`typeDeMusique`) REFERENCES `STYLEDEMUSIQUE` (`typeDeMusique`);
+ALTER TABLE `DORMIR` ADD FOREIGN KEY (`idgroupe`) REFERENCES `GROUPE` (`idgroupe`);
+ALTER TABLE `DORMIR` ADD FOREIGN KEY (`idhebergement`) REFERENCES `HEBERGEMENT` (`idhebergement`);
+ALTER TABLE `RESSEMBLE` ADD FOREIGN KEY (`typeDeMusique`) REFERENCES `STYLEDEMUSIQUE` (`typeDeMusique`);
+ALTER TABLE `PARTICIPE` ADD FOREIGN KEY (`idEvenement`) REFERENCES `EVENEMENT` (`idEvenement`);
+ALTER TABLE `PARTICIPE` ADD FOREIGN KEY (`idGroupe`) REFERENCES `GROUPE` (`idGroupe`);
+ALTER TABLE `EVENEMENT` ADD FOREIGN KEY(`idScene`) REFERENCES `SCENE` (`idScene`);
+ALTER TABLE `EVENEMENT` ADD FOREIGN KEY(`idJournee`) REFERENCES `JOURNEE` (`idJournee`);
+ALTER TABLE `EVENEMENT` ADD FOREIGN KEY(`idConcert`) REFERENCES `CONCERT` (`idConcert`);
+ALTER TABLE `EVENEMENT` ADD FOREIGN KEY(`idActivite`) REFERENCES `ACTIVITE` (`idActivite`);
+ALTER TABLE `APPARTIENT` ADD FOREIGN KEY(`idGroupe`) REFERENCES `GROUPE` (`idGroupe`);
+ALTER TABLE `APPARTIENT` ADD FOREIGN KEY(`idMusicien`) REFERENCES `MUSICIEN` (`idMusicien`);
+
+
+
+
+
+
+
+-- Trigger permettant à un evenement d'avoir seulement un type : soit un concert, soit une activité. 
+delimiter |
+create or replace trigger seulement_1_type_evenement before insert on EVENEMENT for each row 
+begin
+    IF ( NEW.idConcert is not NULL  and NEW.idActivite is not NULL ) THEN
+
+        signal SQLSTATE '45000' set MESSAGE_TEXT = "Il n'est pas possible pour un événement d'avoir deux types";
+
+    end IF;
+end |
+
+
+
+-- Trigger permettant d'éviter qu'une scène est 2 événements renseigné sur elle même en même temps.
+delimiter |
+create or replace trigger evenement_pas_sur_2_scene_en_meme_temps before insert on EVENEMENT for each row
+begin
+declare cpt int;
+    SELECT COUNT(*)
+    INTO cpt
+    FROM EVENEMENT
+    WHERE idScene = NEW.idScene
+    AND DATE_FORMAT(dateDebutE,"%Y-%m-%d %H:%i:%S" ) <= DATE_FORMAT(NEW.dateFinE,"%Y-%m-%d %H:%i:%S")
+    AND DATE_FORMAT(dateFinE,"%Y-%m-%d %H:%i:%S") >= DATE_FORMAT(NEW.dateDebutE,"%Y-%m-%d %H:%i:%S");
+    
+    IF cpt > 0 THEN
+        signal SQLSTATE '45000' set MESSAGE_TEXT = "Un événement existe déjà sur la même scène au même moment";
+    END IF;
+end |
+
+
+
+-- Trigger permettant qu'un événement est bien ses dates comprisent dans la journée ou il se déroule.
+delimiter |
+create or replace trigger evenement_present_dans_la_bonne_journee before insert on EVENEMENT for each row
+begin
+    declare v_day_start DATE;
+    declare v_day_end DATE;
+    -- Obtenir la date de début et de fin de la journée associée à l'événement
+    SELECT DATE_FORMAT(dateDebutJ, "%Y-%m-%d"),  DATE_FORMAT(dateFinJ, "%Y-%m-%d")
+    INTO v_day_start, v_day_end
+    FROM JOURNEE
+    WHERE idJournee = NEW.idJournee;
+    
+    IF DATE_FORMAT(NEW.dateDebutE, "%Y-%m-%d") < v_day_start OR DATE_FORMAT(NEW.dateFinE, "%Y-%m-%d") > v_day_end THEN
+        signal SQLSTATE '45000' set MESSAGE_TEXT = "L'événement doit se dérouler entièrement dans la journée associée.";
+    END IF;
+end |
+
+
+
+-- Trigger permettant de mettre à jour la capacité de l'hebergement quand des personnes reservent des chambres pour celui-ci.
+delimiter |
+create trigger mise_a_jour_capacite_hebergement after insert on DORMIR for each row
+begin
+    declare v_total_reserved INT;
+    -- Obtenir le nombre total de personnes réservées pour cet hébergement
+    SELECT SUM(nbPersonneGroupe) INTO v_total_reserved
+    FROM DORMIR
+    WHERE idhebergement = NEW.idhebergement;
+    
+    -- Mettre à jour la capacité de l'hébergement
+    UPDATE HEBERGEMENT
+    SET nbplace = nbplace - v_total_reserved
+    WHERE idhebergement = NEW.idhebergement;
+end |
+
+
+
+-- Trigger permettant de vérifier que le nombre de personne voulant reserver un hotel ne depasse la capacité d'accueil de cet hotel.
+delimiter |
+create or replace trigger check_capacite_hebergement before insert on DORMIR for each row
+begin
+    declare v_capacity int;
+    -- Obtenir la capacité de l'hébergement choisi
+    SELECT nbplace INTO v_capacity
+    FROM HEBERGEMENT
+    WHERE idhebergement = NEW.idhebergement;
+
+    IF NEW.nbPersonneGroupe > v_capacity THEN
+        signal SQLSTATE '45000' set MESSAGE_TEXT = "La capacité d'hébergement n'est pas suffisante pour ce groupe.";
+    END IF;
+end |
+
+
+
+-- Trigger permettant de vérifier que la date d'une journée se trouve bien entre les dates du festival qui lui est associé.
+delimiter |
+create trigger date_journee_comprise_dans_le_festival before insert on JOURNEE for each row
+begin
+    declare v_festival_start DATE;
+    declare v_festival_end DATE;
+
+    -- Obtenir la date de début et de fin du festival associé
+    SELECT datedebut, datefin
+    INTO v_festival_start, v_festival_end
+    FROM FESTIVAL
+    WHERE idfestival = NEW.idfestival;
+
+    IF NEW.dateDebutJ < v_festival_start OR NEW.dateFinJ > v_festival_end THEN
+        signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Les dates de la journée doivent être comprises entre les dates du festival associé.';
+    END IF;
+end |
+
+
+
+-- Trigger permettant d'interdir à un spectateur d'acheter 2 fois le même billet pour le même événement.
+delimiter |
+create trigger seulement_1_billet_pour_1_evenement before insert on BILLETEVENEMENT for each row
+begin
+    DECLARE v_ticket_count int;
+
+    -- Vérifier si le spectateur a déjà acheté un billet pour le même événement
+    SELECT COUNT(*) INTO v_ticket_count
+    FROM BILLETEVENEMENT
+    WHERE idspectateur = NEW.idspectateur AND idEvenement = NEW.idEvenement;
+
+    IF v_ticket_count > 0 THEN
+        signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Un spectateur ne peut pas acheter deux fois le même billet pour le même événement.';
+    END IF;
+end |
+
+
+-- Trigger permettant d'interdir à un spectateur d'acheter 2 réservations d'une journée en même temps.
+delimiter |
+create trigger seulement_1_reservation_pour_1_journee before insert on RESERVER for each row
+begin
+    DECLARE v_ticket_count int;
+
+    -- Vérifier si le spectateur a déjà acheté un billet pour le même événement
+    SELECT COUNT(*) INTO v_ticket_count
+    FROM RESERVER
+    WHERE idspectateur = NEW.idspectateur AND idJournee = NEW.idJournee;
+
+    IF v_ticket_count > 0 THEN
+        signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Un spectateur ne peut pas réserver deux fois la même journée.';
+    END IF;
+end |
---
+++ BD/insFestKaplia.sql
@@ -0,0 +1,277 @@
+
+INSERT INTO FESTIVAL (idfestival, datedebut, datefin)
+VALUES
+    (1, '2023-07-15', '2023-07-19'),
+    (2, '2023-08-15', '2023-08-20');
+
+
+INSERT INTO SCENE (idScene, nomScene, lieux)
+VALUES (1, 'Main Stage', 'Central Park');
+
+INSERT INTO SCENE (idScene, nomScene, lieux)
+VALUES (2, 'Rock Stage', 'West Field');
+
+INSERT INTO SCENE (idScene, nomScene, lieux)
+VALUES (3, 'Pop Stage', 'East Field');
+
+INSERT INTO SCENE (idScene, nomScene, lieux)
+VALUES (4, 'Electronic Stage', 'South Field');
+
+INSERT INTO SCENE (idScene, nomScene, lieux)
+VALUES (5, 'Mixt Stage', 'South Field');
+
+INSERT INTO SCENE (idScene, nomScene, lieux)
+VALUES (6, 'HardStyle Stage', 'South Field');
+
+
+INSERT INTO JOURNEE (idJournee, nomJournee, dateDebutJ, dateFinJ, idfestival)
+VALUES
+    (1, 'Opening Day', '2023-07-15', '2023-07-15', 1),
+    (2, 'Rock Day', '2023-07-16', '2023-07-16', 1),
+    (3, 'Pop Day', '2023-07-17', '2023-07-17', 1),
+    (4, 'Electronic Day', '2023-08-18', '2023-08-18', 2),
+    (5, 'Closing Day', '2023-08-19', '2023-08-19', 2);
+
+
+INSERT INTO ACTIVITE (idActivite, nomactivite, privatitation)
+VALUES
+    (1, 'Concert', 'Publique'),
+    (2, 'Rencontre avec les artistes', 'Privée'),
+    (3, 'Atelier de musique', 'Publique'),
+    (4, 'Conférence', 'Publique'),
+    (5, 'Exposition', 'Publique');
+
+INSERT INTO CONCERT (idConcert, demontage, montage)
+VALUES
+    (1, 30, 60),
+    (2, 120, 120),
+    (3, 45 , 60),
+    (4, 60 , 120);
+
+
+
+
+INSERT INTO EVENEMENT (idEvenement, dateDebutE, dateFinE, idScene, idJournee, idActivite, idConcert)
+VALUES (2, '2023-07-17 18:00:00', '2023-07-17 20:00:00', 3, 3, NULL, 2);
+
+INSERT INTO EVENEMENT (idEvenement, dateDebutE, dateFinE, idScene, idJournee, idActivite, idConcert)
+VALUES (3, '2023-08-18 22:00:00', '2023-08-18 23:00:00', 2, 4, NULL, 3);
+
+INSERT INTO EVENEMENT (idEvenement, dateDebutE, dateFinE, idScene, idJournee, idActivite, idConcert)
+VALUES (4, '2023-08-19 12:00:00', '2023-08-19 14:00:00', 1, 5, 1, NULL);
+
+
+INSERT INTO EVENEMENT (idEvenement, dateDebutE, dateFinE, idScene, idJournee, idActivite, idConcert)
+VALUES
+(10, '2023-07-15 19:00:00', '2023-07-15 20:00:00', 5, 1, NULL, 1);
+
+INSERT INTO EVENEMENT (idEvenement, dateDebutE, dateFinE, idScene, idJournee, idActivite, idConcert)
+VALUES
+(9, '2023-07-15 21:00:00', '2023-07-15 23:00:00', 5, 1, 2, NULL);
+
+
+
+INSERT INTO SPECTATEUR (idspectateur, nom, numerotel, mail,motsDePasse)
+VALUES
+    (1, 'Spectateur 1', '1234567890', 'spectateur1@example.com',"151654161246"),
+    (3, 'Spectateur 3', '1464565465', 'spectateur3@example.com',"161684321315"),
+    (4, 'Spectateur 4', '9461616160', 'spectateur4@example.com',"1019465031354"),
+    (5, 'Spectateur 5', '6494198848', 'spectateur5@example.com',"25406034841649"),
+    (2, 'Spectateur 2', '9876543210', 'spectateur2@example.com',"60164849410148");
+
+INSERT INTO GROUPE (idgroupe, nomDuGroupe, description, reseausocial, photo, nbpersonne) VALUES
+(1,'Les Rolling Stones', 'Un groupe de rock britannique formé à Londres en 1962.', 'twitter.com/rollingstones', NULL, 4);
+
+INSERT INTO GROUPE (idgroupe, nomDuGroupe, description, reseausocial, photo, nbpersonne) VALUES
+(2,'The Beatles', 'Un groupe de rock britannique formé à Liverpool en 1960.', 'twitter.com/thebeatles', NULL, 4);
+
+INSERT INTO GROUPE (idgroupe, nomDuGroupe, description, reseausocial, photo, nbpersonne) VALUES
+(3,'AC/DC', 'Un groupe de hard rock australien formé à Sydney en 1973.', 'twitter.com/acdc', NULL, 4);
+
+INSERT INTO GROUPE (idgroupe, nomDuGroupe, description, reseausocial, photo, nbpersonne) VALUES
+(4,'Metallica', 'Un groupe de thrash metal américain formé à Los Angeles en 1981.', 'twitter.com/metallica', NULL, 4);
+
+INSERT INTO GROUPE (idgroupe, nomDuGroupe, description, reseausocial, photo, nbpersonne) VALUES
+(5,'Pink Floyd', 'Un groupe de rock progressif britannique formé à Londres en 1965.', 'twitter.com/pinkfloyd', NULL, 4);
+
+INSERT INTO PARTICIPE (idEvenement, idGroupe)
+VALUES
+    (10, 1),
+    (2, 1),
+    (3, 3),
+    (4, 4);
+
+
+
+INSERT INTO RESERVER (idspectateur, idJournee)
+VALUES
+    (1, 1),
+    (1, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5);
+
+INSERT INTO BILLETEVENEMENT (idspectateur, idEvenement)
+VALUES
+    (1, 10),
+    (1, 3),
+    (3, 3),
+    (4, 4);
+
+
+INSERT INTO STYLEDEMUSIQUE (typeDeMusique) VALUES
+    ("Rock"),
+    ("Hard Rock"),
+    ("Trash Metal"),
+    ("Rock Progressif");
+
+
+INSERT INTO STYLE (idgroupe, typeDeMusique) VALUES 
+    (1,"Rock"),
+    (2,"Rock"),
+    (3,"Hard Rock"),
+    (4,"Trash Metal"),
+    (5,"Rock Progressif"); 
+
+INSERT INTO RESSEMBLE (typeDeMusique, typeDeMusique2) VALUES
+    ("Rock","Rock Progressif"),
+    ("Hard Rock","Trash Metal");
+
+INSERT INTO INSTRUMENT(idinstrument, nominstrupent) VALUES
+    (1, "Guitare"),
+    (2, "Chant"),
+    (3,"Basse"),
+    (4,"Batterie");
+
+INSERT INTO MUSICIEN (idMusicien, nom, adresseMail, numeroTelMusicien) VALUES
+    (1, 'Mick Jagger', 'mick@rollingstones.com', 0612345678),
+
+    (2, 'Keith Richards', 'keith@rollingstones.com', 0678901234),
+
+    (3, 'Charlie Watts', 'charlie@rollingstones.com', 0654321098),
+
+    (4, 'Ronnie Wood', 'ronnie@rollingstones.com', 0665432109),
+
+    (5, 'John Lennon', 'john@beatles.com', 0612345778),
+
+    (6, 'Paul McCartney', 'paul@beatles.com', 0677901234),
+
+    (7, 'George Harrison', 'george@beatles.com', 0654321099),
+
+    (8, 'Ringo Starr', 'ringo@beatles.com', 0765432109),
+
+    (9, 'Angus Young', 'angus@acdc.com', 0662345678),
+
+    (10, 'Malcolm Young', 'malcolm@acdc.com', 0678911234),
+
+    (11, 'Brian Johnson', 'brian@acdc.com', 0674321098),
+
+    (12, 'Cliff Williams', 'cliff@acdc.com', 0666432109),
+
+    (13, 'James Hetfield', 'james@metallica.com', 0612345618),
+
+    (14, 'Lars Ulrich', 'lars@metallica.com', 0678901334),
+
+    (15, 'Kirk Hammett', 'kirk@metallica.com', 0654331098),
+
+    (16, 'Robert Trujillo', 'robert@metallica.com', 0667432109),
+
+    (17, 'Roger Waters', 'roger@pinkfloyd.com', 0712345678),
+
+    (18, 'David Gilmour', 'david@pinkfloyd.com', 0678901214),
+
+    (19, 'Richard Wright', 'richard@pinkfloyd.com', 0654821098),
+
+    (20, 'Nick Mason', 'nick@pinkfloyd.com', 0765432110);
+
+INSERT INTO APPARTIENT(idGroupe, idMusicien) VALUES
+    (1,1),
+    (1,2),
+    (1,3),
+    (1,4),
+    (2,5),
+    (2,6),
+    (2,7),
+    (2,8),
+    (3,9),
+    (3,10),
+    (3,11),
+    (3,12),
+    (4,13),
+    (4,14),
+    (4,15),
+    (4,16),
+    (5,17),
+    (5,18),
+    (5,19),
+    (5,20);
+
+
+INSERT INTO JOUE (idgroupe, idinstrument, idMusicien) VALUES 
+    (1, 1, 1),
+
+    (1, 2, 2),
+
+    (1, 3, 3),
+
+    (1, 4, 4),
+
+    (2, 1, 5),
+
+    (2, 2, 6),
+
+    (2, 3, 7),
+
+    (2, 4, 8),
+
+    (3, 1, 9),
+
+    (3, 2, 10),
+
+    (3, 3, 11),
+
+    (3, 4, 12),
+
+    (4, 1, 13),
+
+    (4, 2, 14),
+
+    (4, 3, 15),
+
+    (4, 4, 16),
+
+    (5, 1, 17),
+
+    (5, 2, 18),
+
+    (5, 3, 19),
+
+    (5, 4, 20);
+
+INSERT INTO HEBERGEMENT(idhebergement, nbplace, lieux) VALUES
+    (1, 150, "Manhattan"),
+    (2, 100, "Queens"),
+    (3, 80, "Bronks"),
+    (4, 200, "Brooklyn");
+
+INSERT INTO DORMIR(nbPersonneGroupe, dateArrivee, dateDepart, idgroupe, idhebergement) VALUES
+    (4, '2023-07-15', '2023-07-19', 1, 1),
+    (4, '2023-07-15', '2023-07-19', 2, 2),
+    (4, '2023-07-15', '2023-07-19', 3, 4),
+    (4, '2023-07-15', '2023-07-19', 4, 3),
+    (4, '2023-07-15', '2023-07-19', 5, 1);
+    
+
+
+
+INSERT INTO PRESENT (idfestival, idgroupe, datearriver, datedepart) VALUES
+    (1,1,'2023-07-15','2023-07-19'),
+    (1,2,'2023-07-16','2023-07-19'),
+    (2,3,'2023-08-15','2023-08-19'),
+    (2,4,'2023-08-17','2023-08-19'),
+    (1,5,'2023-07-15','2023-07-19'),
+    (2,5,'2023-08-18','2023-08-19');
+
+
+
+
