e088f0787111460c262d02ea2c97ea1875a41fce
Blandeau Erwan <erwan.blandeau28@gmail.com>
2023-11-16 11:33:27 +0100

    création de la connexion de la base de donnée en ligne

---
+++ loginSQL.py
@@ -0,0 +1,16 @@
+login = "blandeau_oscar" # pip install pymysql
+passwd = "festiutOscar"
+serveur= "mysql-blandeau.alwaysdata.net"
+bd = "blandeau_festivale"
+
+def getLogin():
+    return login
+
+def getPasswd():
+    return passwd
+
+def getServeur():
+    return serveur
+
+def getBd():
+    return bd
